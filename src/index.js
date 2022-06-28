861518d9705abd3f6707ed421ee4ccad25cbf1d0
whidy <whidy@whidy.net>
2022-06-28 20:17:36 +0800

    test fetch blog

--- src/index.js
+++ src/index.js
@@ -1,42 +1,43 @@
 const fs = require("fs");
 // const path = require("path");
-// const axios = require("axios").default;
-const { data } = require("./api-result.json");
-const core = require("@actions/core");
-const {
-  updateAndParseCompoundParams,
-  commitReadme,
-  truncateString,
-  buildReadme,
-  exec,
-  getParameterisedTemplate,
-  escapeHTML,
-} = require("./utils");
+const axios = require("axios").default;
+console.log(process.env.STRAPI_API_TOKEN_RSS);
+// const { data } = require("./api-result.json");
+axios.get('https://admin.whidy.net/api/posts?pagination[page]=1&pagination[pageSize]=5&sort=id:desc&fields[0]=title&fields[1]=updatedAt&fields[2]=slug', {
+  headers: {
+    Authorization: `Bearer ${process.env.STRAPI_API_TOKEN_RSS}`
+  }
+}).then(res => {
+  console.log('result:')
+  console.log(res)
+})
+// const {
+//   updateAndParseCompoundParams,
+//   commitReadme,
+//   truncateString,
+//   buildReadme,
+//   exec,
+//   getParameterisedTemplate,
+//   escapeHTML,
+// } = require("./utils");
 // const { data } = await axios.post('http://localhost:1337/api/auth/local', {
 //   identifier: 'reader@strapi.io',
 //   password: 'strapi',
 // });
 // console.log(data);
-console.log(__dirname);
-console.log(process.env.STRAPI_API_TOKEN_RSS);
-const md = data
-  .map((item, index) => {
-    return `- [${item.attributes.title}](${item.attributes.url})`;
-  })
-  .join("\n");
-const readmeData = fs.readFileSync('README_TEST.md', "utf8");
-const newReadme = buildReadme(readmeData, `\n${md}\n`);
-console.log(newReadme);
+// console.log(__dirname);
+// const md = data
+//   .map((item, index) => {
+//     return `- [${item.attributes.title}](${item.attributes.url})`;
+//   })
+//   .join("\n");
+// const readmeData = fs.readFileSync('README_TEST.md', "utf8");
+// const newReadme = buildReadme(readmeData, `\n${md}\n`);
+// console.log(newReadme);
 // fs.writeFileSync(outputFilePath, JSON.stringify(postsArray), { encoding: 'utf-8'});
 // const res = fs.writeFileSync("/Users/baishun/webs/whidy/NOTE.md", md, {
 //   encoding: "utf-8",
 // });
 
 // console.log(res);
-// axios.get('https://admin.whidy.net/api/posts?pagination[page]=1&pagination[pageSize]=5&sort=id:desc&fields[0]=title&fields[1]=updatedAt&fields[2]=slug', {
-//   headers: {
-//     Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjU2MzkyNzIyLCJleHAiOjE2NTg5ODQ3MjJ9.fcceMhBQBib6PZOEg9PRlUZI8vIXj_akNViLVyPjcgM"
-//   }
-// }).then(res => {
-//   console.log(res)
-// })
+
